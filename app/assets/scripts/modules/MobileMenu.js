4a4dd4d1714ad3890929c19c341058dd2eb9fcb2
Anup Prasad <anupkumarprasad522@gmail.com>
2020-07-19 22:06:46 +0530

    Completed the reveal on scroll behavior.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,6 @@
 class MobileMenu {
     constructor() {
-      this.menuIcon = document.querySelector(".site-header__menu-icon")  /* document.querySelecetor is used to refer the DOM element*/
+      this.menuIcon = document.querySelector(".site-header__menu-icon")  /* document.querySelecetor is used to refer the DOM element. It will select the element matching the paramter given*/
       this.menuContent = document.querySelector(".site-header__menu-content")
       this.siteHeader = document.querySelector(".site-header")
       this.events()
